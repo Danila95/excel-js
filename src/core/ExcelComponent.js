b4de0ed22ed99454cefaf54a5df089a30b41144e
ddb4 <ddb4@tpu.ru>
2023-07-14 12:35:10 +0700

    feat: output template components
    configurate alias

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,9 @@
+import { DomListener } from '@core/DomListener'
+
+// Класс который ... . Наследуется от DomListener
+export class ExcelComponent extends DomListener {
+    // Возвращает шаблон компонента
+	toHTML() {
+		return ''
+	}
+}
